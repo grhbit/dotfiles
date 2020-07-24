1c8875b620e7d0689268e0c33219f4042b155bec
Gwon Seonggwang <g.passcode@gmail.com>
2020-07-24 12:07:45 +0900

    Set `ASDF_SKIP_RESHIM=1` and update tools

--- private_dot_config/zsh/init.zsh
+++ private_dot_config/zsh/init.zsh
@@ -18,6 +18,7 @@ typeset -U path
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
 export ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
 export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="${XDG_CONFIG_HOME}/asdf/global-tool-versions"
+export ASDF_SKIP_RESHIM=1
 source "${ASDF_DATA_DIR}/asdf.sh"
 fpath=(${ASDF_DIR}/completions $fpath)
 
